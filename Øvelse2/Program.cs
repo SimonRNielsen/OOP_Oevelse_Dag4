d0dacea5c59fab0af71ee43a716dc224d7a8ddc9
Simon Nielsen <simonrn@gmail.com>
2024-10-24 11:38:41 +0200

    Øvelse 2

--- Øvelse2/Program.cs
+++ Øvelse2/Program.cs
@@ -10,6 +10,26 @@ namespace Øvelse2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int[] test = new int[1];
+                test[1] = 1;
+                int[] test2 = null;
+                test2[1] = 1;
+            }
+            catch (NullReferenceException n)
+            {
+                Console.WriteLine(n.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Jobs done!");
+            }
+            Console.ReadKey();
         }
     }
 }
