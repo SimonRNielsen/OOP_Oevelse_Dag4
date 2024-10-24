9c4cb9696412790c0188cfdc6233b3f2655449a3
Simon Nielsen <simonrn@gmail.com>
2024-10-24 12:52:05 +0200

    Start øvelse 4

---
+++ Mathstuff/SimpleMath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathstuff
+{
+    /// <summary>
+    /// Contains simple math equators
+    /// </summary>
+    public class SimpleMath
+    {
+        /// <summary>
+        /// Adds 2 integers
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Added result</returns>
+        public int AddInts(int a, int b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Subtracts 2 integers
+        /// </summary>
+        /// <param name="a">Number to subtract from</param>
+        /// <param name="b">Number to subtract</param>
+        /// <returns>Subtracted result</returns>
+        public int SubtractInts(int a, int b)
+        {
+            return a - b;
+        }
+        /// <summary>
+        /// Divides 2 integers
+        /// </summary>
+        /// <param name="a">Number to be divided</param>
+        /// <param name="b">Number to divide with</param>
+        /// <returns>Divided result</returns>
+        public int DividedInts(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ TestLibrary/SimpleMath.cs
@@ -0,0 +1,42 @@
+﻿namespace TestLibrary
+{
+    /// <summary>
+    /// Contains simple math equators
+    /// </summary>
+    public class SimpleMath
+    {
+        /// <summary>
+        /// Adds 2 integers
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Added result</returns>
+        public int AddInts(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        /// <summary>
+        /// Subtracts 2 integers
+        /// </summary>
+        /// <param name="a">Number to subtract from</param>
+        /// <param name="b">Number to subtract</param>
+        /// <returns>Subtracted result</returns>
+        public int SubtractInts(int a, int b)
+        {
+            int c = a - b;
+            return c;
+        }
+        /// <summary>
+        /// Divides 2 integers
+        /// </summary>
+        /// <param name="a">Number to be divided</param>
+        /// <param name="b">Number to divide with</param>
+        /// <returns>Divided result</returns>
+        public int DividedInts(int a, int b)
+        {
+            int c = a / b;
+            return c;
+        }
+    }
+}
